aaa28a0c2300f09d821e900802daaa0dd0433049
omarkamal62 <okamal34@gmail.com>
2024-10-25 22:34:59 +0300

    adding product

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    DB_URI: "mongodb://localhost:27017/buyitnow",
+    API_URL: "http://localhost:3000",
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
 
 export default nextConfig;
---
+++ pages/api/products/index.js
@@ -0,0 +1,15 @@
+import nc from "next-connect";
+import dbConnect from "@/backend/config/dbConnect";
+import {
+  getProducts,
+  newProduct,
+} from "@/backend/controllers/productController";
+
+const handler = nc();
+
+dbConnect();
+
+handler.post(newProduct);
+handler.get(getProducts);
+
+export default handler;
